966958df0bda40a6eaf2e32763254bb9499077df
from <zhangxinzhe@edu.cn>
2018-11-14 16:38:54 +0800

    this shows the disadvantage of namespace use

--- 18.17.cpp
+++ 18.17.cpp
@@ -1,14 +1,22 @@
+#include<iostream>
 
 namespace Exercise{
 	int ivar = 0;
 	double dvar = 0;
 	const int limit = 1000;
 }
+//first:
+//using namespace Exercise;
+//error : reference to ivar is ambigious
 
 int ivar = 0;
 
 int main()
 {
+	//second:
+	using namespace Exercise;
+	//error : reference to ivar(the local one is ambigious);
+
 	//original ver : could not find limit
 	double dvar = 3.14;
 	int iobj = limit + 1;
