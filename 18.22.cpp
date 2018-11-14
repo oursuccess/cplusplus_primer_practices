53ea4b9529584b33994221d2a5b1ac051e2014c7
from <zhangxinzhe@edu.cn>
2018-11-14 18:35:11 +0800

    the order of construct

---
+++ 18.22.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class A{
+	public:
+	A(){ std::cout << "A" << endl; }
+};
+class B : public A{
+	public:
+	B(){cout << "B" << endl;}
+};
+class C : public B{
+	public:
+	C() { cout << "C" << endl;}
+};
+
+class X{
+	public:
+	X() { cout << "X" << endl;}
+};
+class Y{
+	public:
+	Y() { cout << "Y" << endl;}
+};
+class Z:public X,public Y{
+	public:
+	Z(){cout << "Z" << endl;}
+};
+class MI: public C, public Z{
+	public:
+	MI() { cout << "MI" << endl;}
+};
+
+int main()
+{
+	MI m;
+
+	system("pause");
+	return 0;
+}
